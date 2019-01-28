a6d301469fd093371316764408d3dbf3806ca32d
steamingtracks <32366207+steamingtracks@users.noreply.github.com>
2019-01-28 14:24:52 +0000

    Add files via upload

---
+++ CubeWorldForums/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Home World"
+
+@app.route("/info")
+def info():
+	return "<h1> Information </h1>"
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
